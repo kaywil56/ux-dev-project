231952dfb58594d53468e2b1198c0b9d4e24de0f
kaylem williams <willkj8@student.op.ac.nz>
2022-10-18 17:29:42 +1300

    feat: display a list of users from random user api

--- devproject/src/App.svelte
+++ devproject/src/App.svelte
@@ -1,7 +1,26 @@
 <script>
+  import { onMount } from "svelte";
+
+  const RANDOM_USER_API_URL = "https://randomuser.me/api/?results=";
+  const USER_AMOUNT = 15;
+
+  let students = [];
+
+  onMount(async () => {
+    const res = await fetch(RANDOM_USER_API_URL + USER_AMOUNT);
+    students = await res.json();
+    students = students.results;
+  });
 </script>
 
 <main>
+  <ul>
+    {#each students as student}
+      <li>
+        {student.name.first + " " + student.name.last}
+      </li>
+    {/each}
+  </ul>
 </main>
 
 <style>
