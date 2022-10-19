112afacfecbb4b3483a201957d6d1a98188556f8
kaylem williams <willkj8@student.op.ac.nz>
2022-10-19 18:54:13 +1300

    feat: add sidebar for student info

--- devproject/src/App.svelte
+++ devproject/src/App.svelte
@@ -2,9 +2,10 @@
   import { onMount } from "svelte";
   import Student from "./lib/Student.svelte";
   import Tally from "./lib/Tally.svelte";
+  import SideBarInfo from "./lib/SideBarInfo.svelte";
 
   const RANDOM_USER_API_URL = "https://randomuser.me/api/?results=";
-  const USER_AMOUNT = 15;
+  const USER_AMOUNT = 30;
 
   let students = [];
 
@@ -16,29 +17,39 @@
 </script>
 
 <main>
-  <header>
-    <h1>Programming 2 - Semester 2</h1>
-    <p>Week 8 | Class 2</p>
-    <time>20/04/2000</time>
-  </header>
-  <Tally />
-  <table>
-    <thead>
-      <tr>
-        <th colspan="2">Students</th>
-      </tr>
-      <tr>
-        <th>First name</th>
-        <th>Last Name</th>
-        <th>Status</th>
-      </tr>
-    </thead>
-    <tbody>
-      {#each students as student, idx}
+  <section>
+    <header>
+      <h1>Programming 2 - Semester 2</h1>
+      <p>Week 8 | Class 2</p>
+      <time>20/04/2000</time>
+    </header>
+    <Tally />
+    <table>
+      <thead>
         <tr>
-          <Student {student} {idx} />
+          <th colspan="2">Students</th>
         </tr>
-      {/each}
-    </tbody>
-  </table>
+        <tr>
+          <th>First name</th>
+          <th>Last Name</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each students as student, idx}
+          <tr>
+            <Student {student} {idx} />
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </section>
+  <SideBarInfo />
 </main>
+
+<style>
+  main {
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+  }
+</style>
---
+++ devproject/src/lib/SideBarInfo.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<section>
+    <h2>Profile</h2>
+</section>
+
+<style>
+    section{
+        height: 100vh;
+        border: 1px solid #333;
+        position: sticky;
+        top: 0;
+    }
+</style>
